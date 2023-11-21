3f72d392c4dc13565eaaf5e6186cb0bc796f5d0d
Leonardo Ramirez <ldramirez@uce.edu.ec>
2023-11-20 20:34:36 -0600

    Primer commit

---
+++ src/main/java/com/uce/edu/Pa2U1P5LrApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P5LrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5LrApplication.class, args);
+	}
+
+}
