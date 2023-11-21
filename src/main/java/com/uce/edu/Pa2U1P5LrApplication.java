1578ac4f2db29de91a7a5ad9e13aae8b8dccefd1
Leonardo Ramirez <ldramirez@uce.edu.ec>
2023-11-20 20:59:32 -0600

    Taller 3

--- src/main/java/com/uce/edu/Pa2U1P5LrApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5LrApplication.java
@@ -8,6 +8,10 @@ public class Pa2U1P5LrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5LrApplication.class, args);
+		System.out.println("Hola Mundo");
+		System.out.println("Soy Leonardo Ramirez");
+		System.err.println("Cambio en la misma Rama");
+		System.out.println("Cambio en una nueva rama taller 3");
 	}
 
 }
