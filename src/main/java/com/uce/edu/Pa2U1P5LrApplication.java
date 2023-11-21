9f270240f29f931faf2754198f60d6a1f261afad
Leonardo Ramirez <ldramirez@uce.edu.ec>
2023-11-20 21:19:03 -0600

    Taller 5

--- src/main/java/com/uce/edu/Pa2U1P5LrApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5LrApplication.java
@@ -1,19 +1,35 @@
 package com.uce.edu;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.uce.edu.ioc.di.Estudiante;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
-public class Pa2U1P5LrApplication {
+
+public class Pa2U1P5LrApplication implements CommandLineRunner{
+	
+	@Autowired
+	private Estudiante estudiante; 
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5LrApplication.class, args);
-		System.out.println("Hola Mundo");
-		System.out.println("Soy Leonardo Ramirez ");
-		System.err.println("Cambio en la misma Rama");
-		System.out.println("Cambio en la misma Rama");
-		System.out.println("Cambio en una nueva rama taller 4");
-		System.err.println("Nuevo Cambio");
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		this.estudiante.setCedula("1723353825");
+		this.estudiante.setNombre("Leonardo");
+		this.estudiante.setApellido("Ramirez");
+		this.estudiante.setSalario(new BigDecimal(1000));
+		System.out.println(estudiante);
 	}
 
 }
